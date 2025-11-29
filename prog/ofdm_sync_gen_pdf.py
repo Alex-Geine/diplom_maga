c7a98c0fbf74323c3b370964c99818658be5eb4c
Alex Geine <mrborisov00@gmail.com>
2025-11-29 21:56:00 +0300

    add draft of the doc

---
+++ prog/ofdm_sync_gen_pdf.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Пример: построение sinc-функций для OFDM
+t = np.linspace(-4, 4, 1000)
+for k in range(-3, 4):
+    y = np.sinc(t - k)
+    plt.plot(t, y, 'b', linewidth=1.5)
+
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.xlabel('t')
+plt.ylabel('h(t)')
+plt.title('OFDM сигнал во временной области')
+plt.savefig('ofdm_time_df.pdf', bbox_inches='tight')
+plt.show()
